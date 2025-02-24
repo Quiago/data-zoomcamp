7a8db6ce29ed11211eeaed653af64188c95b3164
Carlos Quiala <cquiala12@gmail.com>
2025-02-24 18:19:32 +0000

    Add year comparison

--- week4/taxi_rides/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ week4/taxi_rides/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -2,8 +2,25 @@
 
 with trips_data as (
     select * from {{ ref('fact_trips') }}
-)
-select sum(fare_amount) as revenue_quarter_fare, year, quarter
+), quarterly_revenue as (
+select sum(fare_amount) as quarterly_revenue, year, quarter
 from trips_data
 group by year, quarter
-    
+order by year, quarter
+)
+SELECT
+    year,
+    quarter,
+    quarterly_revenue,
+    LAG(quarterly_revenue) OVER (PARTITION BY quarter ORDER BY year) AS previous_year_revenue,
+    CASE
+        WHEN LAG(quarterly_revenue) OVER (PARTITION BY quarter ORDER BY year) IS NULL THEN NULL
+        ELSE
+            (quarterly_revenue - LAG(quarterly_revenue) OVER (PARTITION BY quarter ORDER BY year)) /
+            LAG(quarterly_revenue) OVER (PARTITION BY quarter ORDER BY year) * 100
+    END AS yoy_growth_percentage
+FROM
+    quarterly_revenue
+ORDER BY
+    year,
+    quarter;
