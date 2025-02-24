d06acd2b93ecf4b660a8feeefa34a2c6e6c7ecb6
Carlos Quiala <cquiala12@gmail.com>
2025-02-24 18:09:21 +0000

    Last commit

--- week4/taxi_rides/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ week4/taxi_rides/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -3,7 +3,7 @@
 with trips_data as (
     select * from {{ ref('fact_trips') }}
 )
-select sum(fare_amount) as revenue_quarter_fare, year, quarter , month
+select sum(fare_amount) as revenue_quarter_fare, year, quarter
 from trips_data
-group by year, quarter, month
+group by year, quarter
     
